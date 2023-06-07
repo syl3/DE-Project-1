c3653fdb5fd58c026dcf3b195770a84d11cb85f8
Yuan <yuan2@myfunnow.com>
2023-06-07 11:53:44 +0800

    add amazon redshift infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,55 @@ resource "aws_s3_bucket_acl" "sde-data-lake-acl" {
   acl    = "public-read-write"
 }
 
+# IAM role for Redshift to be able to read data from S3 via Spectrum
+resource "aws_iam_role" "sde_redshift_iam_role" {
+  name = "sde_redshift_iam_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "redshift.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess", "arn:aws:iam::aws:policy/AWSGlueConsoleFullAccess"]
+}
+
+# Set up Redshift
+resource "aws_redshift_cluster" "sde_redshift_cluster" {
+  cluster_identifier  = "sde-redshift-cluster"
+  master_username     = var.redshift_user
+  master_password     = var.redshift_password
+  port                = 5439
+  node_type           = var.redshift_node_type
+  cluster_type        = "single-node"
+  iam_roles           = [aws_iam_role.sde_redshift_iam_role.arn]
+  skip_final_snapshot = true
+}
+
+# Create Redshift spectrum schema
+provider "redshift" {
+  host     = aws_redshift_cluster.sde_redshift_cluster.dns_name
+  username = var.redshift_user
+  password = var.redshift_password
+  database = "dev"
+}
+
+# External schema using AWS Glue Data Catalog
+resource "redshift_schema" "external_from_glue_data_catalog" {
+  name  = "spectrum"
+  owner = var.redshift_user
+  external_schema {
+    database_name = "spectrum"
+    data_catalog_source {
+      region                                 = var.aws_region
+      iam_role_arns                          = [aws_iam_role.sde_redshift_iam_role.arn]
+      create_external_database_if_not_exists = true
+    }
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,33 @@ variable "bucket_prefix" {
   type        = string
   default     = "sde-data-lake-"
 }
+
+
+## AWS Redshift credentials and node type
+variable "redshift_user" {
+  description = "AWS user name for Redshift"
+  type        = string
+  default     = "sde_user"
+}
+
+variable "redshift_password" {
+  description = "AWS password for Redshift"
+  type        = string
+  default     = "sdeP0ssword0987"
+}
+
+variable "redshift_node_type" {
+  description = "AWS Redshift node  type"
+  type        = string
+  default     = "dc2.large"
+}
+
+
+
+# Setting as budget monitor, so we don't go over 10 USD per month
+resource "aws_budgets_budget" "cost" {
+  budget_type  = "COST"
+  limit_amount = "10"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+}
